39e68880fb105f5d26ad5a5e25c3120fdb20b882
Chan-Young Lee <chan9908181@gmail.com>
2022-08-18 00:00:14 +0200

    Initial Commit

---
+++ Shared/SquareDraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareDraftApp.swift
+//  Shared
+//
+//  Created by Chan-Young Lee on 17.08.22.
+//
+
+import SwiftUI
+
+@main
+struct SquareDraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
